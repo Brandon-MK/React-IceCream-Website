4f2e32b0084673df6643e8bf4a998ce53571c13e
Brandon-MK <moshkalinaki@gmail.com>
2020-10-27 01:25:14 +0300

    Updates

---
+++ precache-manifest.37b1dde69c98651b6a5408809a6417de.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a796d8d7dd0bf1dc40b2dc4824c3f866",
+    "url": "/React-IceCream-Website/index.html"
+  },
+  {
+    "revision": "1563e40280a2047c19ea",
+    "url": "/React-IceCream-Website/static/css/2.3cf5d70f.chunk.css"
+  },
+  {
+    "revision": "c7eadebdaf604eecf03b",
+    "url": "/React-IceCream-Website/static/css/main.8c4af0b5.chunk.css"
+  },
+  {
+    "revision": "1563e40280a2047c19ea",
+    "url": "/React-IceCream-Website/static/js/2.4a16a91c.chunk.js"
+  },
+  {
+    "revision": "10b8ff80054103890476fceb0d8d669d",
+    "url": "/React-IceCream-Website/static/js/2.4a16a91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7eadebdaf604eecf03b",
+    "url": "/React-IceCream-Website/static/js/main.ec43f48f.chunk.js"
+  },
+  {
+    "revision": "8f7393c0ab41292abaa2",
+    "url": "/React-IceCream-Website/static/js/runtime-main.6fa6936c.js"
+  },
+  {
+    "revision": "3b049b2dba0428267e94f39a5ee6fb2c",
+    "url": "/React-IceCream-Website/static/media/1.3b049b2d.jpg"
+  },
+  {
+    "revision": "eed64ee53c15bdf3553f83c08cb7e7ef",
+    "url": "/React-IceCream-Website/static/media/BananaHoney.eed64ee5.png"
+  },
+  {
+    "revision": "0bf56669444d7211570f84286b126f65",
+    "url": "/React-IceCream-Website/static/media/Brambleberry.0bf56669.png"
+  },
+  {
+    "revision": "397bbea94ca31b6fc3dbd59a19e133a7",
+    "url": "/React-IceCream-Website/static/media/Caramel.397bbea9.png"
+  },
+  {
+    "revision": "7a10ce325dcfe51a35701395b11f28fc",
+    "url": "/React-IceCream-Website/static/media/HazelnutCookies.7a10ce32.png"
+  },
+  {
+    "revision": "d6e16f0332189f094ef385cdf4c139a1",
+    "url": "/React-IceCream-Website/static/media/Lime.d6e16f03.png"
+  },
+  {
+    "revision": "fa5b713ca685bed5c98b79def42e9c22",
+    "url": "/React-IceCream-Website/static/media/Pistachio.fa5b713c.png"
+  },
+  {
+    "revision": "e9e5fe82f0a5637ebd5e58cbeb1f558a",
+    "url": "/React-IceCream-Website/static/media/Rasberry.e9e5fe82.png"
+  },
+  {
+    "revision": "d8898e07e0642b5b0514952b06eaa944",
+    "url": "/React-IceCream-Website/static/media/VanillaStrawberry.d8898e07.png"
+  },
+  {
+    "revision": "f48f3ad70f45f358b584fda8276a9e0f",
+    "url": "/React-IceCream-Website/static/media/cherryCheeseCake.f48f3ad7.png"
+  },
+  {
+    "revision": "fa5a4fd931f5baae092b6e55cf29c7fe",
+    "url": "/React-IceCream-Website/static/media/choclate.fa5a4fd9.png"
+  },
+  {
+    "revision": "1157da2a25f92a6f75289960721be290",
+    "url": "/React-IceCream-Website/static/media/ice-cream.1157da2a.jpg"
+  },
+  {
+    "revision": "48d33c85d5333c1210c2f0f7950bb14b",
+    "url": "/React-IceCream-Website/static/media/icecream.48d33c85.jpg"
+  },
+  {
+    "revision": "ad483491221c498ac8a209acf8511538",
+    "url": "/React-IceCream-Website/static/media/icecream2.ad483491.png"
+  },
+  {
+    "revision": "b89ec10dd5d2764984638144f3114013",
+    "url": "/React-IceCream-Website/static/media/icecream2.b89ec10d.jpg"
+  },
+  {
+    "revision": "a23167f5ae9448825ac0c28d55fed9f0",
+    "url": "/React-IceCream-Website/static/media/oreo.a23167f5.png"
+  },
+  {
+    "revision": "4ca09f2bdc98e2643675ae86eb4f7dfd",
+    "url": "/React-IceCream-Website/static/media/place.4ca09f2b.jpg"
+  },
+  {
+    "revision": "268564bfd15e8c288b5eebf2913d9517",
+    "url": "/React-IceCream-Website/static/media/strawberry.268564bf.png"
+  }
+]);
